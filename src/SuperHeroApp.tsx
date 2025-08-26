7acf6188d498ab9753600b25afae6eb75d3ee153
JhessReyes <jhessaureyes@gmail.com>
2025-08-26 12:49:05 -0600

    add configs to fetch

--- src/SuperHeroApp.tsx
+++ src/SuperHeroApp.tsx
@@ -2,11 +2,15 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { RootStackNavigator } from './presentation/navigation/RootStackNavigator';
 import { ThemeAppContext } from './presentation/contexts/ThemeAppContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 export const SuperHeroApp = () => {
   return (
-    <ThemeAppContext>
-      <RootStackNavigator />
-    </ThemeAppContext>
+    <QueryClientProvider client={queryClient}>
+      <ThemeAppContext>
+        <RootStackNavigator />
+      </ThemeAppContext>
+    </QueryClientProvider>
   );
 };
